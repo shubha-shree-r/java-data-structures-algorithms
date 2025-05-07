168100b62d701110a2e5dca697a7491a3a1a401e
shubha-shree-r <shubhashree1805@gmail.com>
2025-05-07 12:26:11 +0530

    bubbleSort

---
+++ src/problemSolving/easy/BubbleSort.java
@@ -0,0 +1,25 @@
+package problemSolving.easy;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    static String doubleSort(int[] intArray){
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = i+1; j < intArray.length ; j++) {
+                if (intArray[i] > intArray[j]){
+                    int num = intArray[i];
+                    intArray[i] = intArray[j];
+                    intArray[j] = num;
+                }
+
+            }
+
+        }
+        return Arrays.toString(intArray);
+    }
+
+    public static void main(String[] args) {
+        int[] input = {2,6,4,8};
+        System.out.println(doubleSort(input));
+    }
+}
