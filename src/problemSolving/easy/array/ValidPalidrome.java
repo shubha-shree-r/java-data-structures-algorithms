136e60a3b28644c0360dbc2575be4299121d8137
Shubha Jeevan <shubhajeevana@Shubhas-Mac-mini.local>
2025-08-19 18:16:40 +0530

    valid palindrome

---
+++ src/problemSolving/easy/array/ValidPalidrome.java
@@ -0,0 +1,26 @@
+package src.problemSolving.easy.array;
+
+public class ValidPalidrome {
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start <= end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+}
